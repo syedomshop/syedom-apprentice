79fbbd9598e1fb3114d56ad9c8ac91da0a858ecf
syedomshop <syedomshop@gmail.com>
2026-03-08 13:08:22 +0500

    Update adminConfig.ts

--- src/lib/adminConfig.ts
+++ src/lib/adminConfig.ts
@@ -1,6 +1,6 @@
 // Add your admin email addresses here
 export const ADMIN_EMAILS = [
-  "admin@syedomlabs.com",
+  "admin@syedomlabs.com", "syedomshop@gmail.com"
   // Add more admin emails as needed
 ];
 
